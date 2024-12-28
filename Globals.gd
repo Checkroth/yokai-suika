5e71fd64c685893eaa9cff1ebf9bd9e5bf264491
Charles Heckroth <checkroth@gmail.com>
2024-12-28 21:09:05 +0900

    Adds debug resources to allow testing while building

--- Globals.gd
+++ Globals.gd
@@ -1,8 +1,22 @@
 extends Node
 
-enum Droppables { DEBUG_SPHERE, MEDAMA_OYAJI }
+# enum Droppables { DEBUG_SPHERE, MEDAMA_OYAJI }
+enum Droppables { DEBUG1, DEBUG2, DEBUG3, DEBUG4, DEBUG5, DEBUG6, DEBUG7, DEBUG8, DEBUG9, DEBUG10}
 
-const DROPPABLE_DATA:Dictionary = {
-	Droppables.DEBUG_SPHERE: preload("res://data/DebugSphere.tres"),
-	Droppables.MEDAMA_OYAJI: preload("res://data/MedamaOyaji.tres")
+#const DROPPABLE_DATA:Dictionary = {
+#	Droppables.DEBUG_SPHERE: preload("res://data/DebugSphere.tres"),
+#	Droppables.MEDAMA_OYAJI: preload("res://data/MedamaOyaji.tres")
+#}
+
+const DROPPABLE_DATA = {
+	Droppables.DEBUG1: preload("res://data/DebugData/DebugSphere.tres"),
+	Droppables.DEBUG2: preload("res://data/DebugData/DebugSphere2.tres"),
+	Droppables.DEBUG3: preload("res://data/DebugData/DebugSphere3.tres"),
+	Droppables.DEBUG4: preload("res://data/DebugData/DebugSphere4.tres"),
+	Droppables.DEBUG5: preload("res://data/DebugData/DebugSphere5.tres"),
+	Droppables.DEBUG6: preload("res://data/DebugData/DebugSphere6.tres"),
+	Droppables.DEBUG7: preload("res://data/DebugData/DebugSphere7.tres"),
+	Droppables.DEBUG8: preload("res://data/DebugData/DebugSphere8.tres"),
+	Droppables.DEBUG9: preload("res://data/DebugData/DebugSphere9.tres"),
+	Droppables.DEBUG10: preload("res://data/DebugData/DebugSphere10.tres")
 }
--- main.gd
+++ main.gd
@@ -19,14 +19,16 @@ func _input(event):
 		print("CLICKED")
 
 func _choose_droppable() -> Droppable:
-	return Globals.DROPPABLE_DATA[Globals.Droppables.DEBUG_SPHERE]
+	return Globals.DROPPABLE_DATA[Globals.Droppables.DEBUG1]
 
 func _upgrade(droppable: Droppable, prev_left: Vector2, prev_right: Vector2):
 	var normalized_x = (prev_left.x + prev_right.x) / 2
 	var normalized_y = (prev_left.y + prev_right.y) / 2
 	var new_suika = droppable.instantiate()
+	new_suika.drop_new.connect(_upgrade)
 	add_child(new_suika)
 	new_suika.position = Vector2(normalized_x, normalized_y)
+	
 
 
 func _spawn_droppable(droppable: Droppable):
